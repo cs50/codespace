40bcc7bfd468a9456087d30fe2526f6e1354bb49
David J. Malan <malan@harvard.edu>
2021-09-07 11:12:38 -0400

    updated prompt

--- etc/profile.d/codespace.sh
+++ etc/profile.d/codespace.sh
@@ -1,6 +1,21 @@
+# If interactive
 if [ "$PS1" ]; then
+
+    # If not root
     if [ "$(whoami)" != "root" ]; then
+
+        # Configure cd to default to workspace
         alias cd="HOME=/workspaces/$RepositoryName cd"
-        PS1='$ '
+
+        # Configure prompt
+        prompt() {
+            local dir="$(dirs +0)" # CWD with ~ for home
+            dir="${dir%/}/" # Remove trailing slash (in case in /) and then re-append
+            dir=${dir#"/workspaces/$RepositoryName/"} # Left-trim workspace
+            dir="${dir} $ " # Add prompt
+            dir=${dir#" "} # Trim leading whitespace (in case in workspace)
+            echo -n "${dir}"
+        }
+        PS1="$(prompt)"
     fi
 fi
