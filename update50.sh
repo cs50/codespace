efd34137ed392251efbb7f4d56cd8c2d57b554b8
David J. Malan <malan@harvard.edu>
2023-04-27 10:35:25 -0400

    updated to cs50.dev

--- update50.sh
+++ update50.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get remote JSON
-remote=$(curl --fail --header "Cache-Control: no-cache" --silent --location https://code.cs50.io/.devcontainer.json)
+remote=$(curl --fail --header "Cache-Control: no-cache" --silent --location https://cs50.dev/.devcontainer.json)
 if [ $? -ne 0 ]; then
     echo "Could not update codespace. Try again later."
     exit 1
