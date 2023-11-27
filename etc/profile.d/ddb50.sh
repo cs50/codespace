122ca056616181eee093a58a50c103a8bff283bb
David J. Malan <malan@harvard.edu>
2023-11-26 23:03:32 -0500

    updated duck

--- etc/profile.d/ddb50.sh
+++ etc/profile.d/ddb50.sh
@@ -1,56 +1,96 @@
-# Dependencies
-if [[ ! $(which ansi2txt) ]]; then
-    echo "Missing ansi2txt"
-    exit 1
-elif [[ ! $(which col) ]]; then
-    echo "Missing col"
-    exit 1
-fi
+DEPENDENCIES=(ansi2txt col)
+STDOUT="/tmp/ddb50.$$"
+ARGV="${STDOUT}.argv"
 
-duck () {
-    if [[ "$1" == "on" ]]; then
-        rm --force "/tmp/noduck"
-    elif [[ "$1" == "off" ]]; then
-        touch "/tmp/noduck"
-        rm --force "/tmp/ddb50.$$" "/tmp/ddb50.$$.argv"
-        unset RUBBERDUCKING
-    elif [[ "$1" == "status" ]]; then
-        if [[ -f "/tmp/noduck" ]]; then
-            echo "off"
-        else
-            echo "on"
-        fi
-    else
-        echo "Usage: duck [off|on|status]"
+# Check for dependencies
+for cmd in ${DEPENDENCIES[@]}; do
+    if ! command -v "$cmd" &>/dev/null; then
+        echo "Missing $cmd"
         exit 1
     fi
-    exit 0
+done
+
+duck() {
+
+    # duck on
+    if [[ $1 == on ]]; then
+        if [[ -z $RUBBERDUCKING ]]; then
+            exec 3>&1 4>&2 # Duplicate file descriptors
+            exec > >(tee -a /tmp/ddb50.$$) 2>&1
+            export RUBBERDUCKING=1
+        fi
+        return 0
+
+    # duck off
+    elif [[ $1 == off ]]; then
+        if [[ ! -z $RUBBERDUCKING ]]; then
+            exec 1>&3 2>&4 # Restore file descriptors
+            exec 3>&- 4>&- # Close file descriptors
+        fi
+        rm --force "$STDOUT" "$ARGV"
+        unset RUBBERDUCKING
+        return 0
+
+    # duck status
+    elif [[ $1 == status ]]; then
+        if [[ -z $RUBBERDUCKING ]]; then
+            echo off
+            return 1
+        else
+            echo on
+            return 0
+        fi
+
+    # duck -h
+    else
+        echo "Usage: duck [off|on|status]"
+        return 1
+    fi
 }
 
-_prompt_command () {
+_prompt_command() {
+
+    # Last command's exit status
     local exit_status=$?
-    if [[ $(duck status) == "on" ]]; then
+
+    # If rubberducking
+    if duck status > /dev/null; then
+
+        # If last command erred
         if [[ $exit_status -ne 0 ]]; then
-            HISTFILE=/tmp/ddb50.$$.argv history -a
-            local argv=$(tail -n 1 /tmp/ddb50.$$.argv)
-            local txt=$(cat "/tmp/ddb50.$$" 2> /dev/null | ansi2txt | col -b | awk '!f && /\\$/ { sub(/\\$/, ""); getline t; $0 = $0 t; } !/\\$/ { f=1 } 1' | tail -n +2)
+
+            # Get command
+            HISTFILE="$ARGV" history -a
+            local argv=$(tail -n 1 "$ARGV")
+
+            # Get command's output
+            local txt=$(cat "$STDOUT" 2> /dev/null) # Redirect stderr in case file doesn't exit somehow
+
+            # Remove any ANSI codes
+            txt=$(echo "$txt" | ansi2txt | col -b)
+
+            # Remove any line continuations in command
+            txt=$(echo "$txt" | awk '!f && /\\$/ { sub(/\\$/, ""); getline t; $0 = $0 t; } !/\\$/ { f=1 } 1')
+
+            # Remove command itself from output
+            txt=$(echo "$txt" | tail -n +2)
+
+            # TODO: call help50 with these values
             if [[ ! -z "$txt" ]]; then
                 echo "$txt" > /tmp/log
                 echo "$argv" > /tmp/log.argv
                 echo "$PWD" > /tmp/log.pwd
             fi
         fi
-        echo -n > "/tmp/ddb50.$$"
-        echo -n > "/tmp/ddb50.$$.argv"
-        echo -n > "/tmp/ddb50.$$.pwd"
+
+        # Flush logs
+        truncate -s 0 "$STDOUT" "$ARGV"
     fi
 }
-
-if [[ $(duck status) == "on" ]]; then
-    if [[ -z "$RUBBERDUCKING" ]]; then
-        exec > >(tee -a "/tmp/ddb50.$$") 2>&1
-        export RUBBERDUCKING="/tmp/ddb50.$$"
-    fi
-fi
-
 export PROMPT_COMMAND=_prompt_command
+
+# Turn duck off when shell exits
+trap 'duck off' EXIT
+
+# Turn duck on by default
+duck on
