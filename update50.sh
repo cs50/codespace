46d4a2812f7608813c37223149d71c82ac4f4817
Rongxin Liu <rongxinliu.dev@gmail.com>
2024-01-28 18:23:09 -0500

    added dev mode for update50

--- update50.sh
+++ update50.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
+# Initialize variables
+URL="https://cs50.dev/devcontainer.json"
+DEVELOP_MODE=false
+
+# Check for flags
+if [ "$1" == "--develop" ]; then
+    URL="https://raw.githubusercontent.com/cs50/codespace/develop/devcontainer.json"
+    DEVELOP_MODE=true
+fi
+
 # Get remote JSON
-remote=$(curl --fail --header "Cache-Control: no-cache" --silent --location https://cs50.dev/devcontainer.json)
+remote=$(curl --fail --header "Cache-Control: no-cache" --silent --location $URL)
 if [ $? -ne 0 ]; then
     echo "Could not update codespace. Try again later."
     exit 1
 fi
 
-# Parse remote JSON
-image=$(echo $remote | jq .image 2> /dev/null)
-regex='"ghcr.io/cs50/codespace:([0-9a-z]*)"'
-if [[ "$image" =~ $regex ]]; then
-    tag="${BASH_REMATCH[1]}"
+if $DEVELOP_MODE; then
+    remote=$(echo "$remote" | sed 's|/\*[^*]*\*/||g')
+    tmpfile=$(mktemp)
+    echo "$remote" | jq 'del(.build, .features) | .image="ghcr.io/cs50/codespace:develop"' > "$tmpfile"
+    mv "$tmpfile" "/workspaces/$RepositoryName/.devcontainer.json"
+    command50 github.codespaces.fullRebuildEnvironment
+    exit 0
 else
-  echo "Could not determine latest version. Try again later."
-  exit 1
+    # Parse remote JSON to get the image tag
+    image=$(echo $remote | jq .image 2> /dev/null)
+    regex='"ghcr.io/cs50/codespace:([0-9a-z]*)"'
+    if [[ "$image" =~ $regex ]]; then
+        tag="${BASH_REMATCH[1]}"
+    else
+        echo "Could not determine latest version. Try again later."
+        exit 1
+    fi
 fi
 
 # Get local version
