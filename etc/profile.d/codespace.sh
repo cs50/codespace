32601f8f9e7931d25d8577af8b6a4aade8e4de9c
Rongxin Liu <rongxinliu.dev@gmail.com>
2024-02-01 15:52:51 -0500

    stop checking env vars (lib50 will)

--- etc/profile.d/codespace.sh
+++ etc/profile.d/codespace.sh
@@ -1,24 +1,6 @@
 # If not root
 if [ "$(whoami)" != "root" ]; then
 
-    # Check each environment variable and echo a warning message if it's not set
-    missing=0
-    env_vars=("CS50_GH_USER" "CS50_TOKEN" "CS50_LANG" "CS50_TZ" "LANG" "LOCAL_WORKSPACE_FOLDER" "TZ")
-    for var in "${env_vars[@]}"; do
-        if [[ -z "${!var}" ]]; then
-            echo -e "\e[31mMissing environment variable: $var\e[0m"
-            missing=1
-        fi
-    done
-
-    if [ $missing -eq 1 ]; then
-        echo -e "\e[31mGitHub username: $GITHUB_USER\e[0m"
-        echo -e "\e[31mCodespace name: $CODESPACE_NAME\e[0m"
-        echo -e "\e[31mBacking repository: https://github.com/code50/$RepositoryName\e[0m"
-        echo -e "\e[31mCommit:\n$(cat /etc/issue)\e[0m"
-        echo "Your codespace doesn't seem to be configured properly. Please copy/paste the above into an email to sysadmins@cs50.harvard.edu so that we can troubleshoot for you!"
-    fi
-
     # Rewrites URLs of the form http://HOST:PORT as https://$CODESPACE_NAME.app.github.dev:PORT
     _hostname() {
 
